f9732f036ab79e6abac931e244e1a4ecf48419ed
kadinu <zxc.zn2006@163.com>
2018-12-18 22:06:56 +0800

    Update CreateDebian.sh

--- CreateDebian.sh
+++ CreateDebian.sh
@@ -24,3 +24,4 @@ rm -rf ~/Chen
 
 sudo apt-get install -y xorg i3 zsh sakura lightdm ttf-wqy-zenhei vim ctags cscope volumeicon-alsa fcitx-sunpinyin dbus-x11 xcompmgr feh conky-all
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="ys"/g' ~/.zshrc
