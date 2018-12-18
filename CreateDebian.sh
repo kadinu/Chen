1a9dc9236281ac3f998b1cf6dc56adc3ed54e1a0
kadinu <zxc.zn2006@163.com>
2018-12-18 20:48:43 +0800

    Update CreateDebian.sh

--- CreateDebian.sh
+++ CreateDebian.sh
@@ -19,6 +19,7 @@ sudo apt-get update
 sudo apt-get install git
 git clone http://github.com/kadinu/Chen.git
 cp -R ~/Chen/.config ~
+cp -R ~/Chen/.vim ~
 
 sudo apt-get install xorg i3 zsh sakura lightdm ttf-wqy-zenhei vim ctags cscope volumeicon-alsa fcitx-sunpinyin dbus-x11 xcompmgr feh conky-all
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
