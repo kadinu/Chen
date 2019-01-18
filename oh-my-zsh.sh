4e9f6a8aababd9a1fc73cfa46eb32eafa2446d81
kadinu <zxc.zn2006@163.com>
2019-01-18 16:09:53 +0800

    Update oh-my-zsh.sh

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+sudo apt-get install -y xorg i3 zsh sakura lightdm ttf-wqy-zenhei volumeicon-alsa fcitx-sunpinyin dbus-x11 xcompmgr feh conky-all
+
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="ys"/g' ~/.zshrc
