ea9264c60cd515a4c4efc1d792c74c8c3398ea5b
kadinu <zxc.zn2006@163.com>
2019-01-21 21:37:57 +0800

    Update oh-my-zsh-install.sh

--- oh-my-zsh-install.sh
+++ oh-my-zsh-install.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
+
+sudo apt-get install -y zsh
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="ys"/g' ~/.zshrc
