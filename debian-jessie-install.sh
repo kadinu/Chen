2fadc3a887fcb1923186c6e5d5501a45bde1713a
kadinu <zxc.zn2006@163.com>
2019-01-21 21:14:19 +0800

    Update debian-jessie-install.sh

--- debian-jessie-install.sh
+++ debian-jessie-install.sh
@@ -3,11 +3,29 @@
 sudo sh -c 'echo "LANG=zh_CN.UTF-8" >> /etc/environment'
 sudo sh -c 'echo "LC_ALL=zh_CN.UTF-8" >> /etc/environment'
 
+# debian 8 jessie sources list
 sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
 sudo touch /etc/apt/sources.list
 sudo sh -c 'echo "deb http://mirrors.ustc.edu.cn/debian/ jessie main contrib non-free" >> /etc/apt/sources.list'
 sudo sh -c 'echo "deb http://security.debian.org/debian-security jessie/updates main contrib non-free" >> /etc/apt/sources.list'
 sudo sh -c 'echo "deb http://mirrors.ustc.edu.cn/debian/ jessie-updates main contrib non-free" >> /etc/apt/sources.list'
+
+# debian 9 stretch sources list
+#sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
+#sudo touch /etc/apt/sources.list
+#sudo sh -c 'echo "deb http://mirrors.ustc.edu.cn/debian/ stretch main contrib non-free" >> /etc/apt/sources.list'
+#sudo sh -c 'echo "deb http://security.debian.org/debian-security stretch/updates main contrib non-free" >> /etc/apt/sources.list'
+#sudo sh -c 'echo "deb http://mirrors.ustc.edu.cn/debian/ stretch-updates main contrib non-free" >> /etc/apt/sources.list'
+
+# raspbian 9 stretch sources list
+#sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
+#sudo touch /etc/apt/sources.list
+#sudo sh -c 'echo "deb http://mirrors.ustc.edu.cn/raspbian/raspbian/ stretch main contrib non-free rpi" >> /etc/apt/sources.list'
+#sudo sh -c 'echo "# deb-src http://mirrors.ustc.edu.cn/raspbian/raspbian/ stretch main contrib non-free rpi" >> /etc/apt/sources.list'
+#sudo mv /etc/apt/sources.list.d/raspi.list /etc/apt/sources.list.d/raspi.list.bak
+#sudo touch /etc/apt/sources.list.d/raspi.list
+#sudo sh -c 'echo "deb http://mirrors.ustc.edu.cn/archive.raspberrypi.org/debian/ stretch main ui" >> /etc/apt/sources.list.d/raspi.list'
+#sudo sh -c 'echo "# deb-src http://mirrors.ustc.edu.cn/archive.raspberrypi.org/debian/ stretch main ui" >> /etc/apt/sources.list.d/raspi.list'
 
 sudo apt-get update
 
