13f6b3941cb16144bdb6e3eafc8e4d97c0fb3f2c
kadinu <zxc.zn2006@163.com>
2019-01-21 21:52:26 +0800

    Update debian-software-install.sh

--- debian-software-install.sh
+++ debian-software-install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+sudo sh -c 'echo "LANG=zh_CN.UTF-8" >> /etc/environment'
+sudo sh -c 'echo "LC_ALL=zh_CN.UTF-8" >> /etc/environment'
 sudo apt-get install -y xorg i3 sakura lightdm ttf-wqy-zenhei volumeicon-alsa fcitx-sunpinyin dbus-x11 xcompmgr feh conky-all
